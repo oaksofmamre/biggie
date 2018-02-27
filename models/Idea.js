9897c2131da9cb37ded2a129df634951792e21f9
DC <heyyodc-git@yahoo.com>
2018-02-26 22:04:31 -0800

    created idea model to store our big ideas

---
+++ models/Idea.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create schema
+const IdeaSchema = new Schema({
+  title: { type: String, required: true },
+  details: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+mongoose.model("Idea", IdeaSchema);
