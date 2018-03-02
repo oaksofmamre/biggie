1d2740eb0e6ed62d3ef76f6ec8ef9900ba87a217
DC <heyyodc-git@yahoo.com>
2018-03-02 00:06:59 -0800

    working on heroku deployment

--- config/database.js
+++ config/database.js
@@ -1,15 +1,8 @@
 "use strict";
-const prodUsername = process.env.PROD_USERNAME;
-const prodPassword = process.env.PROD_PASSWORD;
-const prodHost = process.env.PROD_HOST;
-const prodDb = process.env.PROD_DB;
-
-const prodConnectString = `mongodb://${prodUsername}:${prodPassword}@${prodHost}/${prodDb}`;
 
 if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI: prodConnectString
-  };
+  //process.env.MONGODB_URI is coming from heroku
+  module.exports = { mongoURI: process.env.MONGODB_URI };
 } else {
   module.exports = {
     mongoURI: "mongodb://localhost/biggie-dev"
