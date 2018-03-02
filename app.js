6c74e93afda840fc22ea82d221fc79636e02b029
DC <heyyodc-git@yahoo.com>
2018-03-02 11:47:17 -0800

    deployment work

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const moment = require("moment");
+// const mongoose = require("mongoose");
 
 const app = express();
 
@@ -36,6 +37,18 @@ app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
+//establish database connection
+// app.use((req, res, next) => {
+//   if (mongoose.connection.readyState) {
+//     next();
+//   } else {
+//     require("./mongo")()
+//       .then(() => console.log("MongoDB connected DC!"))
+//       .then(() => next())
+//       .catch(err => console.log(err));
+//   }
+// });
+
 //load routes here
 const ideaRoutes = require("./routes/ideaRoutes");
 //anything that goes to /ideas route will use ideaRoutes.js
@@ -51,7 +64,22 @@ app.get("/", (req, res) => {
   res.redirect("/ideas");
 });
 
+//sever setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Sever listening on localhost:${PORT}`);
+  console.log(`Server listening on localhost:${PORT}`);
 });
+
+// const PORT = process.env.PORT || 3000;
+// const HOST = "localhost";
+
+// let args;
+// process.env.NODE_ENV === "production" ? (args = [PORT]) : (args = [PORT, HOST]);
+
+// args.push(() => {
+//   console.log(`Server listening on ${HOST}:${PORT}`);
+// });
+
+// app.listen.apply(app, args);
+
+// module.exports = app;
--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 "use strict";
 
 if (process.env.NODE_ENV === "production") {
-  //process.env.MONGODB_URI is coming from heroku
-  module.exports = { mongoURI: process.env.MONGODB_URI };
+  //process.env[MONGODB_URI] is coming from heroku
+  module.exports = {
+    mongoURI: process.env[MONGODB_URI]
+  };
 } else {
   module.exports = {
     mongoURI: "mongodb://localhost/biggie-dev"
